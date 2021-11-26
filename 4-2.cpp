0e21a135c9cccf9e7cff2c88faf4a6a5848e266b
ITesserakt <potryas85@mail.ru>
2021-11-26 08:54:46 +0300

    4.2 implemented

--- 4-2.cpp
+++ 4-2.cpp
@@ -8,8 +8,6 @@
 
 #include <cassert>
 #include <iostream>
-#include <vector>
-//#include <rapidcheck.h>
 
 template <typename T, typename Comparator = std::less<T>> class Heap {
 private:
@@ -116,30 +114,13 @@ public:
       sift_down(0);
     }
   }
-
-  void debug() {
-    std::cout << "size = " << size() << ", capacity = " << capacity()
-              << ", elements = {";
-    if (buffer == nullptr)
-      std::cout << "null";
-    else {
-      for (int i = 0; i < capacity() - 1; i++)
-        std::cout << buffer[i] << ", ";
-      std::cout << buffer[capacity() - 1];
-    }
-    std::cout << "}" << std::endl;
-  }
 };
 
 struct User {
   int id;
   int attendance;
 
-  bool operator>(const User &rhs) const { return attendance > rhs.attendance; }
   bool operator<(const User &rhs) const { return attendance < rhs.attendance; }
-  bool operator==(const User &rhs) const {
-    return attendance == rhs.attendance && id == rhs.id;
-  }
 
   friend std::istream &operator>>(std::istream &is, User &user) {
     int id, attendance;
@@ -153,63 +134,33 @@ struct User {
   }
 };
 
-// template <> struct rc::Arbitrary<User> {
-//   static rc::Gen<User> arbitrary() {
-//     return rc::gen::build<User>(
-//         rc::gen::set(&User::id, rc::gen::arbitrary<int>()),
-//         rc::gen::set(&User::attendance, rc::gen::arbitrary<int>()));
-//   }
-// };
-
 template <typename T>
-std::vector<T> get_k_min_elements(T *data, std::size_t n, std::size_t k) {
+void print_k_max_elements(T *data, std::size_t n, std::size_t k) {
   if (k == 0)
-    return {};
+    return;
 
-  Heap<T, std::greater<>> heap;
+  Heap<T> heap;
   heap.grow(k + 1);
 
   for (std::size_t i = 0; i < k; i++)
     heap.template insert(data[i]);
 
   for (std::size_t i = k; i < n; i++) {
-    auto max = heap.top();
-    if (max > data[i]) {
+    auto min = heap.top();
+    if (min < data[i]) {
       heap.pop();
       heap.insert(data[i]);
     }
     assert(heap.size() <= k);
   }
 
-  Heap<T, std::less<>> copy{std::move(heap)};
-  std::vector<T> result{k};
   for (std::size_t i = 0; i < k; i++) {
-    result[i] = copy.top();
-    copy.pop();
+    std::cout << heap.top() << ' ';
+    heap.pop();
   }
-  return result;
 }
 
-// void test() {
-//   rc::check([] {
-//     auto n = *rc::gen::inRange(0, 1'000);
-//     auto k = *rc::gen::inRange(0, n);
-//     auto vec = *rc::gen::uniqueBy<std::vector<User>>(
-//         n, rc::gen::arbitrary<User>(),
-//         [](const User &u) { return u.attendance; });
-//
-//     std::vector<User> sorted = vec;
-//     std::sort(sorted.begin(), sorted.end(), std::less<>());
-//     sorted = {sorted.begin(), sorted.begin() + k};
-//
-//     auto calc = get_k_min_elements(vec.data(), n, k);
-//
-//     for (int i = 0; i < k; i++)
-//       RC_ASSERT(sorted[i] == calc[i]);
-//   });
-// }
-
-void solve() {
+int main() {
   int n, k;
   std::cin >> n >> k;
 
@@ -217,11 +168,7 @@ void solve() {
   for (int i = 0; i < n; i++)
     std::cin >> users[i];
 
-  auto result = get_k_min_elements(users, n, k);
-  for (int i = 0; i < k; i++)
-    std::cout << result[i].id << ' ';
+  print_k_max_elements(users, n, k);
 
   delete[] users;
 }
-
-int main() { solve(); }
